45e2e75ee707d3ee642f10eb854dbe7cffdf5785
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-30 18:25:38 +0000

    add filter to products view model and getter that gets products based on filter values

--- lib/view_models/products_view_model.dart
+++ lib/view_models/products_view_model.dart
@@ -1,11 +1,34 @@
 import '../repositories/products_repository.dart';
 import '../models/product_model.dart';
+import '../enums/collection.dart';
+import '../enums/product_category.dart';
+
+class ProductsFilter {
+  ProductCategory? category;
+  Collections? collection;
+  bool? onSale;
+
+  ProductsFilter({this.category, this.collection, this.onSale});
+}
 
 class ProductsViewModel {
   final ProductsRepository _repository = ProductsRepository();
   late List<Product> products;
+  ProductsFilter filter = ProductsFilter();
 
   ProductsViewModel() {
     products = List<Product>.from(_repository.products);
+  }
+
+  List<Product> get filteredProducts {
+    return products.where((product) {
+      final matchesCategory =
+          filter.category == null || product.category == filter.category;
+      final matchesCollection = filter.collection == null ||
+          product.collections.contains(filter.collection);
+      final matchesOnSale =
+          filter.onSale == null || product.onSale == filter.onSale;
+      return matchesCategory && matchesCollection && matchesOnSale;
+    }).toList();
   }
 }
