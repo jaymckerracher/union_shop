73bf078909f908dc17a515edd49ff6434c81d802
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-30 17:34:28 +0000

    add testing for product repository

---
+++ test/repositories/products_repository_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/repositories/products_repository.dart';
+import 'package:union_shop/models/product_model.dart';
+
+void main() {
+  final repo = ProductsRepository();
+
+  test('should contain 20 products', () {
+    expect(repo.products.length, 20);
+  });
+
+  test('all products should have required fields', () {
+    for (final product in repo.products) {
+      expect(product.id, isNotEmpty);
+      expect(product.title, isNotEmpty);
+      expect(product.description, isNotEmpty);
+      expect(product.price, greaterThan(0));
+      expect(product.imageUrls, isNotEmpty);
+      expect(product.category, isA<ProductCategory>());
+      expect(product.collections, isA<List<Collections>>());
+    }
+  });
+
+  test('sale products should have originalPrice and promoMessage', () {
+    final saleProducts = repo.products.where((p) => p.originalPrice != null);
+    for (final product in saleProducts) {
+      expect(product.promoMessage, isNotNull);
+      expect(product.originalPrice, greaterThan(product.price));
+    }
+  });
+
+  test('no product should have a negative price', () {
+    for (final product in repo.products) {
+      expect(product.price, isNonNegative);
+    }
+  });
+}
