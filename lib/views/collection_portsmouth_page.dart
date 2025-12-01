ee6e1458ff5245f3dd68f9487b938b16b9bca29a
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-01 22:21:13 +0000

    create portsmouth collection page

---
+++ lib/views/collection_portsmouth_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'display_collection.dart';
+import 'app_drawer.dart';
+
+class CollectionPortsmouthPage extends StatelessWidget {
+  const CollectionPortsmouthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: DisplayCollection(filter: 'portsmouth'),
+      drawer: AppDrawer(),
+    );
+  }
+}
