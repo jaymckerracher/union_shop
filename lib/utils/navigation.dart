23aad02d083d1eba02ed7df23a8014bf34459b21
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-01 19:24:25 +0000

    adding navigation function to about page

--- lib/utils/navigation.dart
+++ lib/utils/navigation.dart
@@ -3,3 +3,7 @@ import 'package:flutter/material.dart';
 void navigateToHome(BuildContext context) {
   Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
 }
+
+void navigateToAbout(BuildContext context) {
+  Navigator.pushNamed(context, '/about');
+}
