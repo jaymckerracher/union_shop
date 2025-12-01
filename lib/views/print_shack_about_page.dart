4363798666526fc48a696babb902801e15f8fdbd
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-01 19:36:18 +0000

    create basic print shack about page

---
+++ lib/views/print_shack_about_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'header.dart';
+import 'footer.dart';
+
+class PrintShackAboutPage extends StatelessWidget {
+  const PrintShackAboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Header(),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 48.0),
+            child: Center(
+              child: Text(
+                'Print Shack About',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF444950),
+                ),
+              ),
+            ),
+          ),
+          Footer(),
+        ],
+      ),
+    );
+  }
+}
