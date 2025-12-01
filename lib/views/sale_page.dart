438b9edff03d8c633d0838ea0288678c602b93d0
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-01 22:58:19 +0000

    create sale page

---
+++ lib/views/sale_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'display_collection.dart';
+import 'app_drawer.dart';
+
+class SalePage extends StatelessWidget {
+  const SalePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: DisplayCollection(filter: 'sale'),
+      drawer: AppDrawer(),
+    );
+  }
+}
