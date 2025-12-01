e3bb6621edc9a916071ad8b1b13b60ebfdab4474
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-01 20:50:54 +0000

    create signature and essential collection page

---
+++ lib/views/collection_signature_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'display_collection.dart';
+import 'app_drawer.dart';
+
+class CollectionHalloweenPage extends StatelessWidget {
+  const CollectionHalloweenPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: DisplayCollection(range: 'essential'),
+      drawer: AppDrawer(),
+    );
+  }
+}
