2a0cd9acbd0e1741baf4a00a340db263a57a208f
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-02 15:01:51 +0000

    update display collection to use a normal grid

--- lib/views/display_collection.dart
+++ lib/views/display_collection.dart
@@ -7,97 +7,90 @@ class DisplayCollection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        const SliverToBoxAdapter(child: SizedBox(height: 16)),
-        SliverPadding(
-          padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 32.0),
-          sliver: SliverGrid(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                final product = products[index];
-                return LayoutBuilder(
-                  builder: (context, constraints) {
-                    return GestureDetector(
-                      onTap: () {
-                        navigateToProduct(context, product.id);
-                      },
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Image.asset(
-                                product.imageUrls.first,
-                                fit: BoxFit.cover,
-                                errorBuilder: (context, error, stackTrace) =>
-                                    Container(
-                                  color: Colors.grey[300],
-                                  child: const Center(
-                                    child: Icon(Icons.image_not_supported,
-                                        color: Colors.grey),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            product.title,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                              color: Color(0xFF444950),
-                            ),
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          const SizedBox(height: 4),
-                          Row(
-                            children: [
-                              if (product.originalPrice != null)
-                                Text(
-                                  '£${product.originalPrice!.toStringAsFixed(2)}',
-                                  style: const TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.grey,
-                                    decoration: TextDecoration.lineThrough,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              if (product.originalPrice != null)
-                                const SizedBox(width: 6),
-                              Text(
-                                '£${product.price.toStringAsFixed(2)}',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: product.originalPrice != null
-                                      ? const Color(0xFF4d2963)
-                                      : Colors.black,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                );
-              },
-              childCount: products.length,
-            ),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 16,
-              crossAxisSpacing: 16,
-              childAspectRatio: 0.8,
-            ),
-          ),
+    return Padding(
+      padding:
+          const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 32.0, top: 16.0),
+      child: GridView.builder(
+        itemCount: products.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 16,
+          crossAxisSpacing: 16,
+          childAspectRatio: 0.8,
         ),
-        // Footer removed
-      ],
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return LayoutBuilder(
+            builder: (context, constraints) {
+              return GestureDetector(
+                onTap: () {
+                  navigateToProduct(context, product.id);
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.asset(
+                          product.imageUrls.first,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) =>
+                              Container(
+                            color: Colors.grey[300],
+                            child: const Center(
+                              child: Icon(Icons.image_not_supported,
+                                  color: Colors.grey),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      product.title,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF444950),
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 4),
+                    Row(
+                      children: [
+                        if (product.originalPrice != null)
+                          Text(
+                            '£${product.originalPrice!.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.grey,
+                              decoration: TextDecoration.lineThrough,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        if (product.originalPrice != null)
+                          const SizedBox(width: 6),
+                        Text(
+                          '£${product.price.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: product.originalPrice != null
+                                ? const Color(0xFF4d2963)
+                                : Colors.black,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
