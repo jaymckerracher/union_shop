c476dbe1ce5d0549aa188aea1694546cd77a9310
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-02 22:37:09 +0000

    updating search UI

--- lib/views/search_page.dart
+++ lib/views/search_page.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'header.dart';
 import 'footer.dart';
 import './app_drawer.dart';
+import '../view_models/products_view_model.dart';
+import 'display_collection.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final minHeight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      drawer: const AppDrawer(),
-      body: Container(
-        constraints: BoxConstraints(minHeight: minHeight),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                const Header(),
-                const SizedBox(height: 32),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                  child: TextField(
+    return ChangeNotifierProvider(
+      create: (_) => ProductsViewModel(),
+      child: Scaffold(
+        drawer: const AppDrawer(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Header(),
+              const SizedBox(height: 32),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Consumer<ProductsViewModel>(
+                  builder: (context, viewModel, _) => TextField(
                     decoration: InputDecoration(
                       hintText: 'Search products...',
                       prefixIcon: const Icon(Icons.search),
@@ -30,13 +31,38 @@ class SearchPage extends StatelessWidget {
                         borderRadius: BorderRadius.circular(8.0),
                       ),
                     ),
+                    onChanged: (value) {
+                      viewModel.updateFilter(query: value);
+                    },
                   ),
                 ),
-                const SizedBox(height: 32),
-              ],
-            ),
-            const Footer(),
-          ],
+              ),
+              const SizedBox(height: 32),
+              Consumer<ProductsViewModel>(
+                builder: (context, viewModel, _) {
+                  final products = viewModel.getProducts;
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                        child: Text(
+                          '${products.length} products',
+                          style: const TextStyle(
+                            fontStyle: FontStyle.italic,
+                            color: Colors.black54,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      DisplayCollection(products: products),
+                    ],
+                  );
+                },
+              ),
+              const Footer(),
+            ],
+          ),
         ),
       ),
     );
