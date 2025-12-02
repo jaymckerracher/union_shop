0d46e9a93887191f340e5e2156bb2f8fbafbc1a1
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-02 21:20:24 +0000

    create basic search page

---
+++ lib/views/search_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'header.dart';
+import 'footer.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Header(),
+            const SizedBox(height: 32),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search products...',
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 32),
+            const Footer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
