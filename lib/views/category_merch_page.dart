31445a4aa1ddb74678a1e9f2f34fc01f99c51ebd
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-02 21:13:06 +0000

    matching merchandise page to layout

--- lib/views/category_merch_page.dart
+++ lib/views/category_merch_page.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'display_collection.dart';
 import 'app_drawer.dart';
+import '../view_models/products_view_model.dart';
+import 'header.dart';
+import 'footer.dart';
+import 'filter_sort_menu_bar.dart';
+import '../enums/product_category.dart';
 
 class CategoryMerchPage extends StatelessWidget {
   const CategoryMerchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: DisplayCollection(products: [],),
-      drawer: AppDrawer(),
+    return ChangeNotifierProvider(
+      create: (_) => ProductsViewModel(category: ProductCategory.merchandise),
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Consumer<ProductsViewModel>(
+            builder: (context, viewModel, _) => Column(
+              children: [
+                const Header(),
+                const SizedBox(height: 16),
+                const Text(
+                  'Merch Category',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF222222),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                FilterSortMenuBar(
+                  viewModel: viewModel,
+                  allCollections: true,
+                  allCategories: false,
+                ),
+                DisplayCollection(products: viewModel.getProducts),
+                const Footer(),
+              ],
+            ),
+          ),
+        ),
+        drawer: const AppDrawer(),
+      ),
     );
   }
 }
