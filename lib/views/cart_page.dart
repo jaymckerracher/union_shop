a0003cceea03f6bd40f768f89f765e0b8fdf2703
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-03 20:59:25 +0000

    get sub cart lists in cart view

--- lib/views/cart_page.dart
+++ lib/views/cart_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'header.dart';
 import 'footer.dart';
+import '../view_models/cart_view_model.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Get cart products from the view model (not used yet)
+    final cartViewModel = Provider.of<CartViewModel>(context);
+    final merchItems = cartViewModel.getMerchItems;
+    final clothingItems = cartViewModel.getClothingItems;
+    final printItems = cartViewModel.getPrintItems;
+
     return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
