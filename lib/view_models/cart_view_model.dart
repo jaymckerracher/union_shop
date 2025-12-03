e609ff9dc6cddd6b51bda7f0bd54bdd4eb7c44f3
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-03 20:04:38 +0000

    update merch sub cart in cart view model

--- lib/view_models/cart_view_model.dart
+++ lib/view_models/cart_view_model.dart
@@ -2,44 +2,44 @@ import 'package:flutter/foundation.dart';
 import '../models/cart_item_merch_model.dart';
 
 class CartViewModel extends ChangeNotifier {
-  final List<CartItemMerch> _items = [];
+  final List<CartItemMerch> _merchSubCart = [];
 
-  List<CartItemMerch> get items => List.unmodifiable(_items);
+  List<CartItemMerch> get getMerchItems => List.unmodifiable(_merchSubCart);
 
-  void addItem(CartItemMerch newItem) {
-    final index =
-        _items.indexWhere((item) => item.product.id == newItem.product.id);
+  void addMerchItem(CartItemMerch newItem) {
+    final index = _merchSubCart
+        .indexWhere((item) => item.product.id == newItem.product.id);
     if (index != -1) {
-      final existing = _items[index];
-      _items[index] = CartItemMerch(
+      final existing = _merchSubCart[index];
+      _merchSubCart[index] = CartItemMerch(
         product: existing.product,
         quantity: existing.quantity + newItem.quantity,
       );
     } else {
-      _items.add(newItem);
+      _merchSubCart.add(newItem);
     }
     notifyListeners();
   }
 
-  void removeItem(CartItemMerch itemToRemove) {
-    final index =
-        _items.indexWhere((item) => item.product.id == itemToRemove.product.id);
+  void removeMerchItem(CartItemMerch itemToRemove) {
+    final index = _merchSubCart
+        .indexWhere((item) => item.product.id == itemToRemove.product.id);
     if (index != -1) {
-      final existing = _items[index];
+      final existing = _merchSubCart[index];
       if (existing.quantity > 1) {
-        _items[index] = CartItemMerch(
+        _merchSubCart[index] = CartItemMerch(
           product: existing.product,
           quantity: existing.quantity - 1,
         );
       } else {
-        _items.removeAt(index);
+        _merchSubCart.removeAt(index);
       }
       notifyListeners();
     }
   }
 
-  double get totalPrice {
-    return _items.fold(
-        0.0, (sum, item) => sum + item.product.price * item.quantity);
-  }
+  // double get totalPrice {
+  //   return _items.fold(
+  //       0.0, (sum, item) => sum + item.product.price * item.quantity);
+  // }
 }
