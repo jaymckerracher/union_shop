00441b477f2c0a6a3bc6830ca0546f3adf37bd2f
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-03 20:06:11 +0000

    add functionality for clothing sub cart and print sub cart in cart view model

--- lib/view_models/cart_view_model.dart
+++ lib/view_models/cart_view_model.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/foundation.dart';
 import '../models/cart_item_merch_model.dart';
+import '../models/cart_item_clothing_model.dart';
+import '../models/cart_item_print_model.dart';
 
 class CartViewModel extends ChangeNotifier {
   final List<CartItemMerch> _merchSubCart = [];
+  final List<CartItemClothing> _clothingSubCart = [];
+  final List<CartItemPrint> _printSubCart = [];
 
   List<CartItemMerch> get getMerchItems => List.unmodifiable(_merchSubCart);
+
+  List<CartItemClothing> get getClothingItems =>
+      List.unmodifiable(_clothingSubCart);
+  List<CartItemPrint> get getPrintItems => List.unmodifiable(_printSubCart);
 
   void addMerchItem(CartItemMerch newItem) {
     final index = _merchSubCart
@@ -21,6 +29,40 @@ class CartViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
+  void addClothingItem(CartItemClothing newItem) {
+    final index = _clothingSubCart.indexWhere((item) =>
+        item.product.id == newItem.product.id &&
+        item.size == newItem.size &&
+        item.colour == newItem.colour);
+    if (index != -1) {
+      final existing = _clothingSubCart[index];
+      _clothingSubCart[index] = CartItemClothing(
+        product: existing.product,
+        quantity: existing.quantity + newItem.quantity,
+        size: existing.size,
+        colour: existing.colour,
+      );
+    } else {
+      _clothingSubCart.add(newItem);
+    }
+    notifyListeners();
+  }
+
+  void addPrintItem(CartItemPrint newItem) {
+    final index = _printSubCart.indexWhere((item) => item.id == newItem.id);
+    if (index != -1) {
+      final existing = _printSubCart[index];
+      _printSubCart[index] = CartItemPrint(
+        id: existing.id,
+        print: existing.print,
+        quantity: existing.quantity + newItem.quantity,
+      );
+    } else {
+      _printSubCart.add(newItem);
+    }
+    notifyListeners();
+  }
+
   void removeMerchItem(CartItemMerch itemToRemove) {
     final index = _merchSubCart
         .indexWhere((item) => item.product.id == itemToRemove.product.id);
@@ -33,6 +75,45 @@ class CartViewModel extends ChangeNotifier {
         );
       } else {
         _merchSubCart.removeAt(index);
+      }
+      notifyListeners();
+    }
+  }
+
+  void removeClothingItem(CartItemClothing itemToRemove) {
+    final index = _clothingSubCart.indexWhere((item) =>
+        item.product.id == itemToRemove.product.id &&
+        item.size == itemToRemove.size &&
+        item.colour == itemToRemove.colour);
+    if (index != -1) {
+      final existing = _clothingSubCart[index];
+      if (existing.quantity > 1) {
+        _clothingSubCart[index] = CartItemClothing(
+          product: existing.product,
+          quantity: existing.quantity - 1,
+          size: existing.size,
+          colour: existing.colour,
+        );
+      } else {
+        _clothingSubCart.removeAt(index);
+      }
+      notifyListeners();
+    }
+  }
+
+  void removePrintItem(CartItemPrint itemToRemove) {
+    final index =
+        _printSubCart.indexWhere((item) => item.id == itemToRemove.id);
+    if (index != -1) {
+      final existing = _printSubCart[index];
+      if (existing.quantity > 1) {
+        _printSubCart[index] = CartItemPrint(
+          id: existing.id,
+          print: existing.print,
+          quantity: existing.quantity - 1,
+        );
+      } else {
+        _printSubCart.removeAt(index);
       }
       notifyListeners();
     }
