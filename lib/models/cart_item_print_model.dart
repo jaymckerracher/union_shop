72951b345405488057f645799723ba1e9aa93b16
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-03 20:00:25 +0000

    added uuid to the print model

--- lib/models/cart_item_print_model.dart
+++ lib/models/cart_item_print_model.dart
@@ -1,10 +1,12 @@
 import 'print_model.dart';
 
 class CartItemPrint {
+  final String id;
   final Print print;
   final int quantity;
 
   CartItemPrint({
+    required this.id,
     required this.print,
     required this.quantity,
   });
