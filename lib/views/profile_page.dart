61dec56b44a43abcfa9b98200736c26bd7d26fc7
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-04 18:17:13 +0000

    update page to use single child scroll view to fix scrolling bug

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -10,64 +10,58 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       drawer: AppDrawer(),
-      body: Column(
-        children: [
-          Header(),
-          Expanded(
-            child: Center(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 32.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Icon(
-                        Icons.person_outline,
-                        size: 120,
-                        color: Colors.grey,
-                      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Header(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 32.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Icon(
+                      Icons.person_outline,
+                      size: 120,
+                      color: Colors.grey,
                     ),
-                    SizedBox(height: 32),
-                    Text(
-                      'Username',
-                      style: TextStyle(fontSize: 14, color: Colors.grey),
-                    ),
-                    SizedBox(height: 4),
-                    Text(
-                      'John Smith',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 24),
-                    Text(
-                      'Email',
-                      style: TextStyle(fontSize: 14, color: Colors.grey),
-                    ),
-                    SizedBox(height: 4),
-                    Text(
-                      'johnsmith@example.com',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 24),
-                    Text(
-                      'Password',
-                      style: TextStyle(fontSize: 14, color: Colors.grey),
-                    ),
-                    SizedBox(height: 4),
-                    Text(
-                      '***********', // Obscured password
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
+                  ),
+                  SizedBox(height: 32),
+                  Text(
+                    'Username',
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    'John Smith',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 24),
+                  Text(
+                    'Email',
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    'johnsmith@example.com',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 24),
+                  Text(
+                    'Password',
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    '***********', // Obscured password
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
             ),
-          ),
-          Footer(),
-        ],
+            Footer(),
+          ],
+        ),
       ),
     );
   }
