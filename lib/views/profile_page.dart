f93e51a00d456388851282fe2715460ffe0f5a04
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-04 22:08:33 +0000

    display user details in profile page

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -2,57 +2,63 @@ import 'package:flutter/material.dart';
 import 'header.dart';
 import 'footer.dart';
 import 'app_drawer.dart';
+import '../utils/firebase_get_user.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = getCurrentUser();
+
     return Scaffold(
       drawer: const AppDrawer(),
       body: SingleChildScrollView(
         child: Column(
           children: [
             const Header(),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 32.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 32.0, vertical: 32.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Center(
+                  const Center(
                     child: Icon(
                       Icons.person_outline,
                       size: 120,
                       color: Colors.grey,
                     ),
                   ),
-                  SizedBox(height: 32),
-                  Text(
+                  const SizedBox(height: 32),
+                  const Text(
                     'Username',
                     style: TextStyle(fontSize: 14, color: Colors.grey),
                   ),
-                  SizedBox(height: 4),
+                  const SizedBox(height: 4),
                   Text(
-                    'John Smith',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    user?.displayName ?? 'Unknown',
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  SizedBox(height: 24),
-                  Text(
+                  const SizedBox(height: 24),
+                  const Text(
                     'Email',
                     style: TextStyle(fontSize: 14, color: Colors.grey),
                   ),
-                  SizedBox(height: 4),
+                  const SizedBox(height: 4),
                   Text(
-                    'johnsmith@example.com',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    user?.email ?? 'Unknown',
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  SizedBox(height: 24),
-                  Text(
+                  const SizedBox(height: 24),
+                  const Text(
                     'Password',
                     style: TextStyle(fontSize: 14, color: Colors.grey),
                   ),
-                  SizedBox(height: 4),
-                  Text(
+                  const SizedBox(height: 4),
+                  const Text(
                     '***********', // Obscured password
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
