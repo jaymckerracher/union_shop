dffb0be8e0bd8995ca90e346746f4ac9b96c9e6b
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-04 18:14:23 +0000

    adding basic data to profile page

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -15,9 +15,46 @@ class ProfilePage extends StatelessWidget {
           Header(),
           Expanded(
             child: Center(
-              child: Text(
-                'profile page',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Username',
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'John Smith',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 24),
+                    Text(
+                      'Email',
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'johnsmith@example.com',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 24),
+                    Text(
+                      'Password',
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      '***********', // Obscured password
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
