45a5571c489ab3cb7f2104fe0fde367d09b0c089
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-12-05 06:20:01 +0000

    update tests for cart item merch model

--- test/models/cart_item_merch_model_test.dart
+++ test/models/cart_item_merch_model_test.dart
@@ -30,4 +30,32 @@ void main() {
     final cartItem = CartItemMerch(product: product, quantity: -5);
     expect(cartItem.quantity, -5);
   });
+
+  test('toMap should return correct map', () {
+    final cartItem = CartItemMerch(product: product, quantity: 2);
+    final map = cartItem.toMap();
+    expect(map['product'], isA<Map<String, dynamic>>());
+    expect(map['quantity'], 2);
+    expect(map['product']['id'], '1');
+    expect(map['product']['title'], 'Test Product');
+  });
+
+  test('fromMap should create correct CartItemMerch', () {
+    final map = {
+      'product': {
+        'id': '1',
+        'title': 'Test Product',
+        'description': 'A product for testing',
+        'price': 9.99,
+        'imageUrls': ['http://example.com/image.png'],
+        'category': 'clothing',
+        'collections': ['halloween'],
+      },
+      'quantity': 3,
+    };
+    final cartItem = CartItemMerch.fromMap(map);
+    expect(cartItem.product.id, '1');
+    expect(cartItem.product.title, 'Test Product');
+    expect(cartItem.quantity, 3);
+  });
 }
